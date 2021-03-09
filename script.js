0090a755b8438d98d0edfc9af56addaf2041feb0
Aaron <aaronthesnowboarder@live.com>
2021-03-08 21:14:45 -0800

    search logic

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let searchBtn=document.querySelector('#searchBtn')
 searchBtn.addEventListener('click', searchBtnFun);
+let userInput=document.getElementById('userInput')
 
 
 function searchBtnFun(){
     window.location.href="./search.html"
+}
+
+let UserSearch =function(search){
+    let apiURL="https://www.loc.gov/" + userInput.value
 }
